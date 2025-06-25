233724b4c796e5ada059fd31b320ad1f4fde7e3d
MG <error4489@gmail.com>
2025-06-26 08:55:32 +0900

    구매내역 페이지를 위한 dto 수정

--- src/main/java/com/realive/dto/order/OrderItemResponseDTO.java
+++ src/main/java/com/realive/dto/order/OrderItemResponseDTO.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderItemResponseDTO {
+
     private long id;
     private long productId;
     private String productName;
