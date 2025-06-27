6444b3e99bf0b71dbc031590346437c36b03c7fe
MG <error4489@gmail.com>
2025-06-27 16:47:44 +0900

    review 등록까진 성공 이후 에러 발생

--- src/main/java/com/realive/controller/review/ReviewController.java
+++ src/main/java/com/realive/controller/review/ReviewController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/reviews")
+@RequestMapping("/api/customer/reviews")
 @Log4j2
 @RequiredArgsConstructor
 public class ReviewController {
