3685002a431ff11342095766594815a12100bdbe
JAVCKICHAN <iluvderry@gmail.com>
2025-07-02 14:33:22 +0900

    [WIP] 고객 리뷰 작업중 7/2 14:33 (front)

--- src/main/java/com/realive/service/review/view/ReviewViewServiceImpl.java
+++ src/main/java/com/realive/service/review/view/ReviewViewServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
